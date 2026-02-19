a09d98b0e34dccc2cd1feb09218a1d7b42bc0d6f
tojvex <tornike.zoidze@gmail.com>
2026-02-19 20:39:55 +0400

    feat: integrate Google Tag Manager for enhanced tracking

--- landing-app/src/app/layout.tsx
+++ landing-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Noto_Sans_Georgian } from "next/font/google";
@@ -30,7 +31,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5GGC4RKK');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${firaGo.variable} ${notoSansGeorgian.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5GGC4RKK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
